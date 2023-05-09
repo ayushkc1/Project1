d61d2ff1aa3e80740373b9e57d2cd978654df9de
dhakalmahima188 <dhakalmahima18@gmail.com>
2023-05-09 16:15:37 +0545

    PLM-15 Home page made and issue renew return options created

--- libraryapp/models.py
+++ libraryapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    Book_id=models.IntegerField(primary_key=True)
+    Book_name=models.CharField(max_length=100)
+    Author=models.CharField(max_length=100)
+    Book_Issue_Date=models.DateField()
+    
--- libraryapp/views.py
+++ libraryapp/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'libraryapp/home.html')
 
 # @login_required(login_url='/login')
 def home(request):
