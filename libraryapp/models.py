ae92db41c81e5868f1589a210759d268a9c25918
dhakalmahima188 <dhakalmahima18@gmail.com>
2023-05-10 11:22:48 +0545

    PLM-22 books added in the database and sucessfully shown in frontend

--- libraryapp/models.py
+++ libraryapp/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Book(models.Model):
-    Book_id=models.IntegerField(primary_key=True)
-    Book_name=models.CharField(max_length=100)
-    Author=models.CharField(max_length=100)
-    Book_Issue_Date=models.DateField()
+    book_id=models.IntegerField(primary_key=True)
+    title=models.CharField(max_length=100)
+    author=models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
+   
+
+
+class Borrow(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_borrowed = models.DateField(auto_now_add=True)
+    due_date = models.DateField()
     
--- libraryapp/views.py
+++ libraryapp/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Book, Borrow
 
-
-def index(request):
-    return render(request, 'libraryapp/home.html')
+# def index(request):
+#     return render(request, 'libraryapp/home.html')
 
 # @login_required(login_url='/login')
 def home(request):
@@ -13,6 +16,24 @@ def home(request):
 
 def issue(request):
     return render(request, 'libraryapp/issue.html')
+
+def index(request):
+    books = Book.objects.all()
+    print("Called?")
+    print(len(books))
+    return render(request, 'libraryapp/home.html', {'books': books})
+
+@login_required
+def issue(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    if request.method == 'POST':
+        Borrow.objects.create(
+            book=book,
+            user=request.user,
+            due_date=request.POST['due_date']
+        )
+        return HttpResponseRedirect(reverse('book_detail', args=(book.id,)))
+    return render(request, 'libraryapp/issue.html', {'book': book})
 
 def login(request):
     if request.method == 'POST':
