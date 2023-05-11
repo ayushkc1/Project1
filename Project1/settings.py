31ce6811532a9fd3c6fa2157e473dd6c95ddd7cc
dhakalmahima188 <dhakalmahima18@gmail.com>
2023-05-11 16:50:26 +0545

    PLM-34 setup and routing of Django REst framework done

--- Project1/settings.py
+++ Project1/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'libraryapp'
+    'libraryapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,11 @@ CSRF_TRUSTED_ORIGINS = [
 ] 
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
